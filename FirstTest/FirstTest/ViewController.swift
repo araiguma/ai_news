dc67fb167f59ca4f7c7892e7291abfef8ac63dc4
新井　祐輝 <y-arai@araiyuuakira-no-MacBook-Air.local>
2016-06-10 16:18:41 +0900

    画像を登録。 アニメーションを追加しようとして失敗。

--- FirstTest/FirstTest/ViewController.swift
+++ FirstTest/FirstTest/ViewController.swift
@@ -12,12 +12,29 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // 【1】 画像データをインスタンス化
+        let imageA: UIImage! = UIImage(named: "Image1-A")
+        let imageB: UIImage! = UIImage(named: "Image1-B")
+        // 【2】 コマ送りに使う画像データの配列をセット
+        イメージ.animationImages = [imageA, imageB]
+        // 【3】 コマ送りの間隔を設定
+        イメージ.animationDuration = 1
+        // 【4】 コマ送りのアニメーションを開始
+        イメージ.startAnimating()
     }
 
+    @IBOutlet weak var イメージ: UIImageView!
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+   
+    @IBAction func クリック戻る(sender: AnyObject) {
+    }
+    
+    @IBAction func クリック進む(sender: AnyObject) {
     }
 }
 
